56e74fb9c1e81c410cad952c78f45ceba4788d55
Andrej Kovacevic <glazba@gmail.com>
2023-01-30 22:40:40 +0100

    Character Visual, Rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,6 +30,8 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * Time.deltaTime * moveSpeed;
 
-        Debug.Log(Time.deltaTime);
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
     }
 }
