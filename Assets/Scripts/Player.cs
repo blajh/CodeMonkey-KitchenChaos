8dc2c02fec262fef014431943e47748055485223
Andrej Kovacevic <glazba@gmail.com>
2023-01-31 12:59:03 +0100

    Interact Action, C# Events

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,30 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private Vector3 lastInteractDirection;
 
+    private void Start() {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (moveDir != Vector3.zero) {
+            lastInteractDirection = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
+        } else {
+            Debug.Log("-");
+        }
+    }
+
     private void Update() {
         HandleMovement();
         HandleInteractions();
@@ -34,7 +58,7 @@ public class Player : MonoBehaviour
         if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
                 //Has ClearCounter
-                clearCounter.Interact();
+                
             }
         } else {
             Debug.Log("-");
