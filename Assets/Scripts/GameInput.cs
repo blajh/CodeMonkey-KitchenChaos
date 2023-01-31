3ed7c301f202a78855efbba4cc3994442502d433
Andrej Kovacevic <glazba@gmail.com>
2023-01-31 13:45:55 +0100

    minor

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -11,7 +11,6 @@ public class GameInput : MonoBehaviour
     private void Awake() {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
-
         playerInputActions.Player.Interact.performed += Interact_performed;
     }
 
